13ef90af8a1ef41f62614647687dda98c2990f3f
alaricespina <alaricsespina@gmail.com>
2022-03-27 19:57:21 +0800

    327-2

---
+++ Cubism/Cubism.js
@@ -0,0 +1,31 @@
+console.log("Script loaded");
+
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(canvas)
+
+var c = canvas.getContext('2d');
+
+
+var colorArray = [
+    '#952E5D',
+    '#E24756',
+    '#351A70',
+    '#FDC64E',
+    '#FC7E4B'
+];
+
+function Cube(x, y, l, w, h, side_length){
+
+}
+
+
+function init(){ 
+
+}
+
+init();
+draw_everything();
+
+
